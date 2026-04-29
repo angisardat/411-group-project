cbe0c5200e237848d45eb8a83e12537cd2f0c6d9
angelisardat <avi5222@psu.edu>
2026-04-29 19:38:45 -0400

    connected server.js to database

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const connectDB = require("./config/database");
 const Student   = require("./models/students.model"); // change this line to new database
 
 const app = express();
+const PORT = 3000;
+
+connectDB();
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
