ae295e3275f5dd73e211556b33e5aba40c32722b
angelisardat <avi5222@psu.edu>
2026-04-29 19:45:51 -0400

    installed express

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/school"); // change this to database name
+    await mongoose.connect("mongodb://127.0.0.1:27017/habit-tracker"); // change this to database name
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
